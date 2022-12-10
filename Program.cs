28ec920de4f56e310a891538e9743b5c399a3a1b
Mohácsi László <mohacsi@altinum.hu>
2022-12-10 20:16:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebAppIdentityTest.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.UseDefaultFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = (context) =>
+    {
+        if (context.Context.User.Identity.IsAuthenticated)
+        {
+            //A kérés küldője be van jelentkezve
+            if (context.Context.Request.Path.Value == "/index.html")
+            {
+                context.Context.Response.Redirect("/titkos/logged_in.html");
+            }
+        }
+        else
+        {
+            //A kérés küldője nincs bejelentkezve
+            if (context.Context.Request.Path.Value.Contains("titkos"))
+            {
+                context.Context.Response.Redirect("/index.html");
+            }
+        }
+    }
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+//app.MapRazorPages();
+
+app.Run();
---
+++ controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace WebAppIdentityTest.controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [Route("user/test")]
+        [Authorize]
+        [HttpGet]
+        public IActionResult Test()
+        {
+            var c = HttpContext.User.Identity.Name;
+            return Ok($"{c} be van jelentkezve");
+        }
+
+        [HttpPost]
+        [Route("user/login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto model)
+        {
+            var c = HttpContext;
+
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                var appUser = _userManager.Users.SingleOrDefault(r => r.UserName == model.UserName);
+                await _signInManager.SignInAsync(appUser, isPersistent: false);
+
+                return Ok(true);
+            }
+            else
+            {
+                return BadRequest(false);
+            }
+        }
+
+        [HttpPost]
+        [Route("user/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok(true);
+        }
+
+        [Route("user/register")]
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] RegisterUserRequest request)
+        {
+            var user = new IdentityUser();
+            user.UserName = request.UserName;
+            user.Email = request.Email;
+            user.Id = Guid.NewGuid().ToString();
+ 
+            //Check wether user exists
+            var u = await _userManager.FindByNameAsync(request.UserName);
+            if (u != null)
+            {
+                return Ok(new RegisterUserResponse() { Success = false, Field = "new_username", Detail = "User name already taken." });
+            }
+
+            //Validate username
+            Regex r = new Regex(@"^[a-zA-Z0-9\-]+$");
+            if (!r.IsMatch(request.UserName))
+            {
+                return Ok(new RegisterUserResponse() { Success = false, Field = "new_username", Detail = "Only characters A-Z, a-z, numbers and '-' are acceptable." });
+            }
+
+            //Trying to create user
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (result.Succeeded)
+            {
+                var newUser = await _userManager.FindByNameAsync(request.UserName);
+                if (newUser == null)
+                {
+                    return Ok(new RegisterUserResponse() { Success = false, Field = "message", Detail = "Error creating new user" });
+                }
+                else
+                {
+                    return Ok(new RegisterUserResponse() { Success = true, Field = "message", Detail = "User successfully added." });
+                }
+            }
+            else
+            {
+                return Ok(new RegisterUserResponse() { Success = false, Field = "message", Detail = string.Join(";", (result.Errors.Select(e => e.Description))) });
+            }
+        }
+    }
+
+    public class RegisterUserRequest
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+
+    /// <summary>
+    /// A regisztrációnel ezen az osztályon keresztül kap tájékoztatát a kliens 
+    /// a művelet eredményéről
+    /// </summary>
+    public class RegisterUserResponse
+    {
+        public bool Success { get; set; }
+
+        public string Field { get; set; } = string.Empty;
+
+        public string Detail { get; set; } = string.Empty;
+    }
+
+    public class LoginDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
